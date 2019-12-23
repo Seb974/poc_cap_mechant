2932422d44701380593fff2b7a81faf8c71e3490
Sébastien Maillot <sebastienmaillot@macbook-pro-de-sebastien.home>
2019-12-23 19:38:26 +0400

    test du collapse

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +13,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ArticleRepository $articleRepository)
+    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository)
     {
         $articles = $articleRepository->findAll();
+        $categories = $categoryRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'articles' => $articles,
+            'categories' => $categories,
         ]);
     }
 }
